ceb785a22f46f17d6b94741c22fe69a515a6dab5
Aleksashka301 <aleksashka301@gmail.com>
2025-06-06 22:27:17 +0500

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,7 +1,10 @@
+import phonenumbers
+
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.templatetags.rest_framework import items
 
 from .models import Product, Order, OrderDetail
 
@@ -61,12 +64,33 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     order_details = request.data
-    if not order_details.get('products'):
-        return Response('Поле products: Обязательное поле с типом list и оно не должно быть пустым!')
-    elif isinstance(order_details['products'], str):
+    product_ids = list(Product.objects.values_list('id', flat=True))
+
+    for key in ['products', 'firstname', 'lastname', 'phonenumber', 'address']:
+        if not key in order_details:
+            return Response(f'Поле {key}: Обязательное поле, не должно быть пустым!')
+
+    for key, detail in items(order_details):
+        if not detail:
+            return Response(f'Поле {key}: Обязательное поле, не должно быть пустым!')
+
+    if not isinstance(order_details['products'], list):
         return Response(
             f'Поле products: Ожидался list со значениями, но был получен "{type(order_details['products'])}".'
         )
+
+    phone = phonenumbers.parse(order_details['phonenumber'], 'RU')
+    if not phonenumbers.is_valid_number(phone):
+        return Response(f'Поле phonenumber: Введён не правильный номер телефона {order_details['phonenumber']}')
+    
+    if not order_details['products'][0]['product'] in product_ids:
+        return Response(f'Поле product: Введён не верный ключ продукта {order_details['products'][0]['product']}')
+
+    for field in ['firstname', 'lastname', 'phonenumber', 'address']:
+        if not isinstance(order_details[field], str):
+            return Response(
+                f'Поле {field}: Ожидалась строка со значением, но был получен "{type(order_details[field])}".'
+            )
 
     order = Order.objects.create(
         address=order_details['address'],
