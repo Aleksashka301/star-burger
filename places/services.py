1a42acd98c262e9c914b8ec4cb458e1ef16f0c13
Aleksashka301 <aleksashka301@gmail.com>
2025-06-18 18:26:52 +0500

    Update services.py

--- places/services.py
+++ places/services.py
@@ -3,20 +3,20 @@ from .models import Place
 from restaurateur.geo import fetch_coordinates
 
 
-def get_or_create_coordinates(api_key, address):
-    try:
-        place = Place.objects.get(address=address)
-        return place.latitude, place.longitude
-    except Place.DoesNotExist:
+def get_or_create_coordinates(api_key, address, address_cache=None):
+    if address_cache is not None and address in address_cache:
+        return address_cache[address]
+
+    place, created = Place.objects.get_or_create(address=address)
+    if created or not place.latitude or not place.longitude:
         coords = fetch_coordinates(api_key, address)
-        if coords is None:
+        if coords:
+            place.latitude, place.longitude = coords
+            place.save()
+        else:
             return None
 
-        lat, lon = coords
-        try:
-            Place.objects.create(address=address, latitude=lat, longitude=lon)
-        except IntegrityError:
-            place = Place.objects.get(address=address)
-            return place.latitude, place.longitude
+    if address_cache is not None:
+        address_cache[address] = (place.latitude, place.longitude)
 
-        return lat, lon
+    return place.latitude, place.longitude
