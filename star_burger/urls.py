e2c16acb72c6503d0c47b69675a5129681093aa7
Aleksashka301 <aleksashka301@gmail.com>
2025-08-03 13:04:01 +0500

    Update urls.py

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
+from restaurateur.views import trigger_error
 from . import settings
 
 urlpatterns = [
@@ -27,6 +28,7 @@ urlpatterns = [
     path('api/', include('foodcartapp.urls')),
     path('manager/', include('restaurateur.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('trigger-error/', trigger_error),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
