a26463459fd397564e796ace0fc436fbac72b5a5
Aleksashka301 <aleksashka301@gmail.com>
2025-08-04 19:43:15 +0500

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -93,14 +93,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('DB_NAME'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': env.str('DB_HOST', 'localhost'),
-        'PORT': env.str('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.parse(env.str('DATABASE_URL'))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
